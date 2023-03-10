a28891a3711a0ac4322c44a7c994e583b7408cd7
Xpronoob <kevin_03_puris@hotmail.com>
2023-03-09 18:27:55 -0600

    Init

---
+++ src/test/java/com/caso/practico/CasoPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caso.practico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
